324aba07ab3faf190483029cb7dd05cacd855661
Caleb Chesnut <caleb@tackk.com>
2014-02-09 22:53:20 -0500

    flex-box

--- js/main.js
+++ js/main.js
@@ -5,30 +5,6 @@ var username01 = "calebchesnut",
     username02 = "stephenpolitte",
     username03 = "joeboyle";
 			
-/*
-function loadProject(projectID) {
-	behanceAPI.getProjectDetails(projectID, function(loadModules) {
-    var moduleHTML = "";
-			
-    var moduleTitle = "";
-    moduleTitle += "<h1>" + loadModules.name + "</h1>";
-    $(".module-title").html(moduleTitle);
-			
-    var moduleDescription = "";
-    moduleDescription += "<p>" + loadModules.description + "</p>";
-    $(".module-description").html(moduleDescription);
-			
-    for(var i=0; i<loadModules.modules.length; i++) {
-	    if(loadModules.modules[i].src!=undefined) {
-	        moduleHTML += "<p><img src='" + loadModules.modules[i].src + "'></p>";
-	    }
-	  }
-	  
-	$(".modules").html(moduleHTML);
-	});
-}
-*/
-			
 $(document).ready(function() {
 	// Set behance api key
 	behanceAPI.setKey(behancekey);
@@ -38,13 +14,36 @@ $(document).ready(function() {
 		var projectHTML = "";
 		for(var i=0; i<projectlist.length; i++) {
 			var project = projectlist[i];
-	  projectHTML += "<li><a href='#' onclick='loadProject(" + project.id + ")'>" + project.name + "</a></li>";
-	  // if you would like to see project thumbnails use this:
-	   projectHTML += "<a href='#' onclick='loadProject(" + project.id + ")'><img src='" + project.covers[202] + "'></a>";
+	  projectHTML += "<li><a href='#' onclick='loadProject(" + project.id + ")'><img src='" + project.covers[202] + "'></a>" + 
+	  "<a href='#' onclick='loadProject(" + project.id + ")'>" + project.name + "</a></li>";
 	}
 	$(".projects").html(projectHTML);
 	});
-	
-	behanceAPI.getProjects(username01, function(projectID){})
-	
 });
+
+
+/*
+
+<script id="entry-template" type="text/x-handlebars-template">
+  template content
+</script>
+
+
+var source   = $("#entry-template").html();
+var template = Handlebars.compile(source);
+
+
+var context = {title: "My New Post", body: "This is my first post!"}
+var html    = template(context);
+
+
+<div class="entry">
+  <h1>My New Post</h1>
+  <div class="body">
+    This is my first post!
+  </div>
+</div>
+
+
+project-id : "project.id" 
+*/
