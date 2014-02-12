7743c53ad49441083d6687e54627a2ff8d147168
Caleb Chesnut <caleb@tackk.com>
2014-02-11 20:29:59 -0500

    added header image and initialized skrollr

--- js/main.js
+++ js/main.js
@@ -30,5 +30,12 @@ $(document).ready(function() {
 
 // Trying out Skrollr
 
-
-var s = skrollr.init();
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
